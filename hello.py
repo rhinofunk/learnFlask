1259f2e83626e166dd190ef92763fd25bc44176d
beaug <rhyminggoat@gmail.com>
2018-05-30 09:47:02 -0400

    again

--- hello.py
+++ hello.py
@@ -3,10 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return '<h1>Hello World!!</h1></p> What\'s up?'
+def hello_world():	
+	return "<h1>Hello World!!</h1>you </p> What\'s down?"
 
 if __name__ == '__main__':
 	host = os.getenv('IP', '0.0.0.0')
 	port = int(os.getenv('PORT', 5000))
+	app.debug = True
 	app.run(host=host, port=port)
